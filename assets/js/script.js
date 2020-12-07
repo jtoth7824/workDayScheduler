d5c77d38c186e75a7c2cb2cf95cc44f4845c8843
LostInSpace <71476446+jtoth7824@users.noreply.github.com>
2020-12-07 15:03:14 -0500

    time functions colorize planner, storage now works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,125 @@
-var events = [{
-    hour: "",
-    eventName: ""
-}];
-
-var timeBlocks = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
-var timeBlock = $(".container");
-
-function retrieveEvents() {
-
-    var storedEvents = JSON.parse(localStorage.getItem("Events"));
-
-    if (storedEvents === null) {
-        localStorage.setItem("Events", JSON.stringify(events));
+var timeBlocks = [{
+        hour: "8am",
+        eventName: "",
+        tftime: 8
+    },
+    {
+        hour: "9am",
+        eventName: "",
+        tftime: 9
+    },
+    {
+        hour: "10am",
+        eventName: "",
+        tftime: 10
+    },
+    {
+        hour: "11am",
+        eventName: "",
+        tftime: 11
+    },
+    {
+        hour: "12pm",
+        eventName: "",
+        tftime: 12
+    },
+    {
+        hour: "1pm",
+        eventName: "",
+        tftime: 13
+    },
+    {
+        hour: "2pm",
+        eventName: "",
+        tftime: 14
+    },
+    {
+        hour: "3pm",
+        eventName: "",
+        tftime: 15
+    },
+    {
+        hour: "4pm",
+        eventName: "",
+        tftime: 16
+    },
+    {
+        hour: "5pm",
+        eventName: "",
+        tftime: 17
     }
-}
+];
 
+console.log(timeBlocks[9].hour, timeBlocks[9].eventName, timeBlocks[9].tftime);
+
+/*var timeBlocks = ["8am", "9am"[, "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];*/
+var timeBlock = $(".container");
 var now = dayjs().format('dddd, MMM DD');
 now = now + "th";
 
 function displayPlanner() {
-    
-        for (j = 0; j <timeBlocks.length; j++) {
-            var planRow = $("<form>");
-            $(planRow).attr("class", "row");
-            $(".container").append(planRow);
 
-            for (var i = 0; i < 3; i++) {
-                if (i === 1) {
-                    
-                    var textBox = $("<textarea>");
-/*                    $(textBox).text("This is the text area");*/
-                    $(textBox).addClass("present");
-                    $(textBox).attr("name", j);
-                    $(textBox).addClass("col-md-9 description");
-                }
-                else if(i===0){
+    for (j = 0; j < timeBlocks.length; j++) {
+        var planRow = $("<form>");
+        $(planRow).attr("class", "row");
+        $(".container").append(planRow);
 
-                    var divEl = $("<div>");
-                    divEl.text(timeBlocks[j]);
-                    $(divEl).addClass("col-md-2 time-block hour");
+        for (var i = 0; i < 3; i++) {
+            if (i === 1) {
+
+                var textBox = $("<textarea>");
+                $(textBox).text(timeBlocks[j].eventName);
+                var currentTime = dayjs().hour();
+
+                switch(parseInt(currentTime) > parseInt(timeBlocks[j].tftime)) {
+                    case true:
+                        $(textBox).addClass("past");
+                        break;
+                    case false:
+                        console.log("equal to" + (parseInt(currentTime) === parseInt(timeBlocks[j].tftime)));
+                        if(parseInt(currentTime) === parseInt(timeBlocks[j].tftime)) {
+                            $(textBox).addClass("present");
+                        }
+                        else {
+                            $(textBox).addClass("future");
+                        }
+                        break;
+                    default:
+                        break;
                 }
-                else{
-                    var savedBtn= $("<button>" + "Save" + "</button>");
-                    $(savedBtn).addClass("saveBtn");
-                    $(savedBtn).val(j);
-                    $(savedBtn).addClass("col-md-1");
-                }
+
+                $(textBox).attr("name", j);
+                $(textBox).addClass("col-md-9 description");
+
+            } else if (i === 0) {
+
+                var divEl = $("<div>");
+                divEl.text(timeBlocks[j].hour);
+                $(divEl).addClass("col-md-2 time-block hour");
+            } else {
+                var savedBtn = $("<button>" + "Save" + "</button>");
+                $(savedBtn).addClass("saveBtn");
+                $(savedBtn).val(j);
+                $(savedBtn).addClass("col-md-1");
             }
-            $(planRow).append(divEl, textBox, savedBtn);
         }
-}
-
-function saveEvents() {
-    localStorage.setItem("Events", JSON.stringify(storedEvents)); 
-    
+        $(planRow).append(divEl, textBox, savedBtn);
+    }
 }
 
 function initialize() {
 
+    // Get stored events from localStorage
+    // Parsing the JSON string to an object
+    var eventstry = JSON.parse(localStorage.getItem("Events"));
+
+    // If events were not retrieved from localStorage, update the events array to it
+    if (eventstry === null) {
+        localStorage.setItem("Events", JSON.stringify(timeBlocks));
+    }
+
     $("#currentDay").text(now);
-    retrieveEvents();
+    timeBlocks = JSON.parse(localStorage.getItem("Events"));
     displayPlanner();
 }
 
@@ -73,15 +133,14 @@ $(".saveBtn").on("click", function (event) {
     eventText = $(this).siblings().next().val();
     whichHour = $(this).siblings().text();
 
-    console.log("whichHour = " + whichHour);
+    timeBlocks[whichBtn].eventName = eventText;
 
-    console.log("saved button = " + whichBtn);
-    console.log(eventText);
-
-   events[0].eventName = eventText;
-   events[0].hour = whichHour;
-    console.log(events.eventName);
-    console.log(events);
-
-    localStorage.setItem("Events", JSON.stringify(events));
+    localStorage.setItem("Events", JSON.stringify(timeBlocks));
 });
+
+function sortFunction() {
+    events.sort(function (a, b) {
+        console.log("in sort function");
+        return b.hour - a.hour;
+    });
+}
